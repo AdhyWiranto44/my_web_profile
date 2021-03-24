a1bfb7739344bd35cd641a02c5e79721cbd7e215
adhywiranto44 <adhywiranto68@gmail.com>
2021-03-24 09:41:18 +0700

    refactoring index.ejs, style.css, and script.js

--- public/js/script.js
+++ public/js/script.js
@@ -47,60 +47,60 @@ function windowScroll(e) {
     }
 }
 
-$(document).ready(function () {
+// $(document).ready(function () {
     
-    // my latest video
-    var key = 'AIzaSyCs2cNnwQbCYd0DKRhfTZPEHVgBmG8ww2E';
-    var playlistId = 'PL76Rc5GEPL9p-shFfNEagPZg3MytMFnBF';
-    var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+//     // my latest video
+//     var key = 'AIzaSyCs2cNnwQbCYd0DKRhfTZPEHVgBmG8ww2E';
+//     var playlistId = 'PL76Rc5GEPL9p-shFfNEagPZg3MytMFnBF';
+//     var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
 
-    var options = {
-        part: 'snippet',
-        key: key,
-        maxResults: 1,
-        playlistId: playlistId
-    }
+//     var options = {
+//         part: 'snippet',
+//         key: key,
+//         maxResults: 1,
+//         playlistId: playlistId
+//     }
 
-    loadVids();
+//     loadVids();
 
-    function loadVids() {
-        $.getJSON(URL, options, function (data) {
-            var id = data.items[0].snippet.resourceId.videoId;
-            mainVid(id);
-            resultsLoop(data);
-        });
-    }
+//     function loadVids() {
+//         $.getJSON(URL, options, function (data) {
+//             var id = data.items[0].snippet.resourceId.videoId;
+//             mainVid(id);
+//             resultsLoop(data);
+//         });
+//     }
 
-    function mainVid(id) {
-        $('#video').html(`
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-				`);
-    }
+//     function mainVid(id) {
+//         $('#video').html(`
+// 					<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+// 				`);
+//     }
 
 		
-    function resultsLoop(data) {
+//     function resultsLoop(data) {
 
-        $.each(data.items, function (i, item) {
+//         $.each(data.items, function (i, item) {
 
-            var thumb = item.snippet.thumbnails.medium.url;
-            var title = item.snippet.title;
-            var desc = item.snippet.description.substring(0, 500);
-            var vid = item.snippet.resourceId.videoId;
+//             var thumb = item.snippet.thumbnails.medium.url;
+//             var title = item.snippet.title;
+//             var desc = item.snippet.description.substring(0, 500);
+//             var vid = item.snippet.resourceId.videoId;
 
-            $('.latestVideoTitle').append(`${title}`);
-            $('.latestVideoDesc').append(`${desc}`);
+//             $('.latestVideoTitle').append(`${title}`);
+//             $('.latestVideoDesc').append(`${desc}`);
 
-        });
-    }
+//         });
+//     }
 
-		// CLICK EVENT
-    $('main').on('click', 'article', function () {
-        var id = $(this).attr('data-key');
-        mainVid(id);
-    });
+// 		// CLICK EVENT
+//     $('main').on('click', 'article', function () {
+//         var id = $(this).attr('data-key');
+//         mainVid(id);
+//     });
 
-});
+// });
 
 // Script Google Form
 const scriptURL = 'https://script.google.com/macros/s/AKfycbybrGlJZOJiqxnQCJQGZlLlfVC1chq7bPAbF9oxdMrTZrRXBcH4arSRHGKg-UfRY8cW/exec';
